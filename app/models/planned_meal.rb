3ed2ebdc52de0cf3930addf605a75f4f5c4f1b73
Bryan Ash <bryan.a.ash@gmail.com>
2010-02-09 21:27:59 -0500

    Refactored to Recipe.suggestion

--- app/models/planned_meal.rb
+++ app/models/planned_meal.rb
@@ -3,14 +3,10 @@ class PlannedMeal < ActiveRecord::Base
   belongs_to :recipe
   belongs_to :meal_plan
 
-  before_create :set_recipe_suggestion
-
-  def set_recipe_suggestion
-    self.recipe = Recipe.first
-  end
+  after_create :get_new_recipe_suggestion
 
   def get_new_recipe_suggestion
-    update_attributes :recipe => Recipe.last
+    update_attribute :recipe, Recipe.suggestion(recipe)
   end
 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,9 @@ class Recipe < ActiveRecord::Base
 
   has_many :ingredient_amounts
 
+  def self.suggestion(current = nil)
+    first unless current
+    last
+  end
+
 end
