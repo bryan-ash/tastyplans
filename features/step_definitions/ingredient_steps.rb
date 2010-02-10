d9a170c98ecc5bfbe7a2e6a390b709631e93e537
Bryan Ash <bryan.a.ash@gmail.com>
2010-02-09 22:59:59 -0500

    Fixed broken ingredient specs

--- features/step_definitions/ingredient_steps.rb
+++ features/step_definitions/ingredient_steps.rb
@@ -6,7 +6,7 @@ When /^I create an ingredient named "(.*)"$/ do |name|
   Given 'I am on the ingredients page'
   When  'I follow "New"'
   Then  'I should see "New Ingredient"'
-  When  'I fill in "name" with "' + name + '"'
+  When  'I fill in "ingredient_name" with "' + name + '"'
   And   'I press "Submit"'
 end
 
@@ -15,6 +15,6 @@ When /^I rename "(.+)" to "(.+)"$/ do |old, new|
   When  'I go to the ingredients page'
   And   'I follow "' + old + '"'
   Then  'I should see "Editing Ingredient"'
-  When  'I fill in "name" with "' + new + '"'
+  When  'I fill in "ingredient_name" with "' + new + '"'
   And   'I press "Submit"'
 end
