3887d6b8f4b46d4acb710aa146f58f3a8fe2505a
Bryan Ash <bryan.a.ash@gmail.com>
2010-03-16 20:44:10 -0400

    Rails 3 scope in Ingredient

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,7 +2,7 @@ class Ingredient < ActiveRecord::Base
 
   default_scope :order => 'name ASC'
 
-  named_scope :named_like, lambda { |name| { :conditions => ['name ILIKE ?', "%#{name}%"] } }
+  scope :named_like, lambda { |name| where('name ILIKE ?', "%#{name}%") }
 
   validates_presence_of   :name
   validates_uniqueness_of :name
