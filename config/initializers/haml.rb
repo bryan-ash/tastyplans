b5158ca91574bf9d70f77d9f316f3b662438f656
Bryan Ash <bryan.a.ash@gmail.com>
2010-12-07 21:24:06 -0500

    Haml initializer overrides :markdown filter to guard against javascript attack

---
+++ config/initializers/haml.rb
@@ -0,0 +1,12 @@
+module Mabel
+  module Filters
+    module Markdown
+      include Haml::Filters::Base
+      lazy_require 'rdiscount'
+
+      def render(text)
+        ::RDiscount.new(text, :filter_html).to_html
+      end
+    end
+  end
+end
