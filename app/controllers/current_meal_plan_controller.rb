7392708328334266babc9e0eea0d4f68ae538993
Bryan Ash <bryan.a.ash@gmail.com>
2011-01-02 00:43:18 -0500

    Added a flash notice when a recipe is added to current meal plan

--- app/controllers/current_meal_plan_controller.rb
+++ app/controllers/current_meal_plan_controller.rb
@@ -3,7 +3,7 @@ class CurrentMealPlanController < ApplicationController
   def add_recipe
     recipe = Recipe.find(params[:id])
     current_user.current_meal_plan(true).planned_meals.create(:recipe => recipe)
-    redirect_to :back
+    redirect_to :back, :notice => "That recipe was added to your current meal plan"
   end
 
   def edit
