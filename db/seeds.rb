7d55a18fc7ff3828366538656f11b364cdc33a72
Bryan Ash <bryan.a.ash@gmail.com>
2011-01-11 21:27:13 -0500

    Added demo user meal plans to db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+demo_user = User.find_by_username("Demo User")
+
+MealPlan.find_or_create_by_name(:name => "Empty Plan", :user => demo_user)
+
+meal_plan = MealPlan.find_or_create_by_name(:name => "Birthday Week", :user => demo_user)
+demo_user.current_meal_plan = meal_plan
+demo_user.save
+
+["Beef and Red Bean Chili",
+ "Devil's Food Layer Cake"
+].each do |recipe_name|
+  recipe = Recipe.find_by_name(recipe_name)
+  meal_plan.planned_meals << PlannedMeal.create(:recipe => recipe)
+  meal_plan.save
+end
