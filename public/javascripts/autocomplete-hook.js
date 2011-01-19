3c26f29861101a32e11a3ddfebadaae8b07855fc
Bryan Ash <bryan.a.ash@gmail.com>
2011-01-18 22:29:50 -0500

    Configure autocomplete to not select by default

--- public/javascripts/autocomplete-hook.js
+++ public/javascripts/autocomplete-hook.js
@@ -3,8 +3,8 @@ jQuery(function($){
   $('input.autocomplete').each(function(){
     var input = $(this);
     input.autocomplete(input.attr('data-autocomplete-url'),{
-      matchContains:1,     //also match inside of strings when caching
-      removeInitialValue:0 //when first applying $.autocomplete
+      matchContains:1,      //also match inside of strings when caching
+      selectFirst:null
     });
   }); 
 });
