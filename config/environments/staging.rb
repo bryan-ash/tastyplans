6e51a781cff46dbe8a48f454af52555c0a8c1e90
Bryan Ash <bryan.a.ash@gmail.com>
2011-02-24 23:44:49 -0500

    added staging environment config

---
+++ config/environments/staging.rb
@@ -0,0 +1,50 @@
+Mabel::Application.configure do
+  # Settings specified here will take precedence over those in config/environment.rb
+
+  # The production environment is meant for finished, "live" apps.
+  # Code is not reloaded between requests
+  config.cache_classes = true
+
+  # Full error reports are disabled and caching is turned on
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+
+  # See everything in the log (default is :info)
+  # config.log_level = :debug
+
+  # Use a different logger for distributed setups
+  # config.logger = SyslogLogger.new
+
+  # Use a different cache store in production
+  # config.cache_store = :mem_cache_store
+
+  # Static serving is required for Heroku
+  config.serve_static_assets = true
+
+  # Enable serving of images, stylesheets, and javascripts from an asset server
+  # config.action_controller.asset_host = "http://assets.example.com"
+
+  # Disable delivery errors, bad email addresses will be ignored
+  # config.action_mailer.raise_delivery_errors = false
+
+  # Enable threaded mode
+  # config.threadsafe!
+
+  config.active_support.deprecation = :log
+
+  config.action_mailer.default_url_options = {
+    :host     => 'mabelmeals.com',
+    :protocol => 'http'
+  }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'mabelmeals.com',
+    :user_name            => 'ohmabel@gmail.com',
+    :password             => 'ablemabel',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
+end
