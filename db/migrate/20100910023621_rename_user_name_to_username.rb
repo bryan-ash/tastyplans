f5f67218933d2f4ba483b8712f3e2ffb09571a26
Bryan Ash <bryan.a.ash@gmail.com>
2013-08-25 22:48:07 -0400

    Fix migration: remove index before renaming

--- db/migrate/20100910023621_rename_user_name_to_username.rb
+++ db/migrate/20100910023621_rename_user_name_to_username.rb
@@ -1,13 +1,13 @@
 class RenameUserNameToUsername < ActiveRecord::Migration
   def self.up
-    rename_column :users, :name, :username
     remove_index  :users, :name
+    rename_column :users, :name, :username
     add_index     :users, :username
   end
 
   def self.down
-    rename_column :users, :username, :name
     remove_index  :users, :username
+    rename_column :users, :username, :name
     add_index     :users, :name
   end
 end
