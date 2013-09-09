b2bbc8613239e055085193985a03370e4243d7dd
Bryan Ash <bryan.a.ash@gmail.com>
2013-09-08 21:23:26 -0400

    Use unobtrusive JavaScript for adding ingredients

--- public/javascripts/active_form.js
+++ public/javascripts/active_form.js
@@ -1,7 +1,27 @@
-function remove_fields(link) {
-  $(link).prev("input[type=hidden]").val("1");
-  $(link).closest("tr").hide();
-}
+var $ingredient_template_html = null;
+
+function grab_ingredient_fields_template() {
+  var ingredient_template = $("tr.ingredient").filter(":first").clone();
+  $ingredient_template_html = ingredient_template.wrap("<div>").parent().html();
+};
+
+function hook_add_ingredient_action() {
+  $("#add-ingredient").click(function(event) {
+    event.preventDefault();
+    var new_id = new Date().getTime();
+    var regexp = new RegExp("0", "g");
+    $("tr:last").after($ingredient_template_html.replace(regexp, new_id));
+  });
+};
+
+function hook_remove_ingredient_action() {
+  $(".remove-ingredient").each(function(){
+    $(this).click(function(event) {
+      event.preventDefault();
+      $(this).closest("tr").hide();
+    });
+  });
+};
 
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
@@ -13,4 +33,8 @@ $(document).ready(function () {
   if (!("autofocus" in document.createElement("input"))) {
     $(":text:visible:enabled:first").focus();
   }
+
+  grab_ingredient_fields_template();
+  hook_add_ingredient_action();
+  hook_remove_ingredient_action();
 });
